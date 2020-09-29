f229a936a392136c8d5e68b76bba3a6165098d0c
Brit Duncan <brit.duncan94@gmail.com>
2020-09-29 15:37:27 +1300

    added php files, can now access vm webpage, added gitignore, moved security groups from outbound to inbound

---
+++ www/convert/convert.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML//EN">
+<html>
+<head><title>Convert Webpage</title>
+<style>
+th { text-align: left; }
+
+table, th, td {
+border: 2px solid grey;
+border-collapse: collapse;
+}
+
+th, td {
+padding: 0.2em;
+}
+</style>
+</head>
+
+<body>
+<h1>Convert webpage</h1>
+
+<p>Showing conversion</p>
+
+
+</body>
+</html>
---
+++ www/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML//EN">
+<html>
+<head><title>Main Webpage</title>
+<style>
+th { text-align: left; }
+
+table, th, td {
+border: 2px solid grey;
+border-collapse: collapse;
+}
+
+th, td {
+padding: 0.2em;
+}
+</style>
+</head>
+
+<body>
+<h1>Main webpage</h1>
+
+<p>Showing conversion options</p>
+
+
+</body>
+</html>
